48694aec52569e9e171758ebc21158de37a0127c
julien <julienchauvin@hotmail.fr>
2024-11-18 21:55:32 +0100

    dev-html Service Portfolio Blog Home

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar"
 
 const Blog = () => {
@@ -5,67 +6,75 @@ const Blog = () => {
         <>
             <header>
                 <Navbar />
+                <div>
+                    <img src="../public/img/banner.jpg" alt="banner théme bleu et lumiére" />
+                </div>
             </header>
             <main>
                 <div>
-                    <h2>Blog</h2>
+                    <h2>BLOG</h2>
                     <span>Retrouver ici quelques articles sur le développement web.</span>
                 </div>
-                <article>
-                    <div>
-                        <img src="../public/blog/coder.jpg" alt="image représentant un écran avec des lignes de code" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 22 août 2022</span>
-                </article>
-                <article>
-                    <div>
-                        <img src="../public/blog/croissance.jpg" alt="image représentant un écran avec des lignes de code" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 20 août 2022</span>
-                </article>
-                <article>
-                    <div>
-                        <img src="../public/blog/google.jpg" alt="image représentant un ordinateur portable sur le navigateur Google" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 22 août 2022</span>
-                </article>
-                <article>
-                    <div>
-                        <img src="../public/blog/coder.jpg" alt="image représentant un écran avec des lignes de code" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 22 août 2022</span>
-                </article>
-                <article>
-                    <div>
-                        <img src="../public/blog/coder.jpg" alt="image représentant un écran avec des lignes de code" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 22 août 2022</span>
-                </article>
-                <article>
-                    <div>
-                        <img src="../public/blog/coder.jpg" alt="image représentant un écran avec des lignes de code" />
-                        <h3>coder son site en HTML/CSS</h3>
-                        <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
-                        <button type="button" class="btn btn-primary">Lire la suite</button>
-                    </div>
-                    <span>Publié le 22 août 2022</span>
-                </article>
+                <div>
+                    <article>
+                        <div>
+                            <img src="../public/blog/coder.jpg" alt="image représentant un écran avec des lignes de code" />
+                            <h3>Coder son site en HTML/CSS</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 22 août 2022</span>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="../public/blog/croissance.jpg" alt="image représentant la croissance d'une plante ayant comme racine des piéces" />
+                            <h3>Vendre ses produits sur le web</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 20 août 2022</span>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="../public/blog/google.jpg" alt="image représentant un ordinateur portable sur le navigateur Google" />
+                            <h3>Se positionner sur Google</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 1 août 2022</span>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="../public/blog/screens.jpg" alt="image représentant un ordinateur de burreau avec un ordinateur portable et une tablette sur une table en bois." />
+                            <h3>Coder en responsive design</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 31 juillet 2022</span>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="../public/blog/seo.jpg" alt="image sur fond bleu avec plusieurs mots écrient de façon horizontale et vertical" />
+                            <h3>Techniques de référencement</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 30 juillet 2022</span>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="../public/blog/technos.jpg" alt="image représentant une grille de couleur avec les différentes marques d'outils de développement web." />
+                            <h3>Apprendre à coder</h3>
+                            <span>Some quick example text to build on the card title and make upthe bulk of the card's content.</span>
+                            <button type="button" class="btn btn-primary">Lire la suite</button>
+                        </div>
+                        <span>Publié le 12 juillet 2022</span>
+                    </article>
+                </div>
             </main>
+            <footer>
+                <Footer />
+            </footer>
         </>
     )
 }
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,12 +1,46 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar"
 
 const Service = () => {
     return(
         <>
-            <h1>
-                Mes services
-            </h1>
-            <Navbar />
+         <header>
+                <Navbar />
+                <div>
+                    <img src="../public/img/banner.jpg" alt="banner théme bleu et lumiére" />
+                </div>
+           </header>
+           <main>
+                <h2>MON OFFRE DE SERVICES</h2>
+                <span>Voici les prestations sur lesquelles je peus intervenir.</span>
+                <div>
+                    <section>
+                        <img src="../pblic/icon/display-solid.svg" alt="icon d'un ordinateur." />
+                        <h3>UX DESIGN</h3>
+                        <p>
+                            L'<strong>UX Design </strong>est une méthode de conception centrée sur l'utilisateur.
+                         Son but est d'offrir une expérience de navigation optimale à l'internaute.
+                        </p>
+                    </section>
+                    <section>
+                        <img src="../pblic/icon/file-code-solid.svg" alt="icon d'un fichier." />
+                        <h3>développement web</h3>
+                        <p>
+                            Le<strong>développement des sites web </strong>repose sur l'utilisation des langages
+                             HTML, CSS, JavaScript et PHP.
+                        </p>
+                    </section>
+                    <section>
+                        <img src="../pblic/icon/magnifying-glass-dollar-solid.svg" alt="icon d'une loupe avec le signe du dollar à l'intérieur." />
+                        <h3>référencement</h3>
+                        <p>Le<strong>référencement naturel d'un site, </strong>aussi appelé SEO, consiste à mettre
+                         des techniques en oeuvre pour améliorer sa position dans les résultats des moteurs de recherche.</p>
+                    </section>
+                </div>
+           </main>
+           <footer>
+                <Footer />
+           </footer>
         </>
     )
 }
