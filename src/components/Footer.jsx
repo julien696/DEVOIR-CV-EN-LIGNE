1585ee9e0028f86235507c4a7c9f83b04aed7131
julien <julienchauvin@hotmail.fr>
2024-11-21 18:51:02 +0100

    css home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,54 +3,58 @@ import { Link } from "react-router-dom"
 export default function Footer() {
     return(
         <>
-            <section>
-                <h5>John Doe</h5>
-                <div>
-                    <span>40 rue Laure Diebold</span><br/>
-                    <span>69009 Lyon, France</span><br/>
-                    <span>Téléphone : 06 20 30 40 50</span>
+            <div className="d-flex flex-column mt-5 pt-5 pb-3 footer">
+                <div className="px-3">
+                    <section>
+                        <h5>John Doe</h5>
+                        <div>
+                            <span>40 rue Laure Diebold</span><br/>
+                            <span>69009 Lyon, France</span><br/>
+                            <span>Téléphone : 06 20 30 40 50</span>
+                        </div>
+                        <div>
+                            <a href="https://github.com" target="_blank">
+                                <img src="../public/icon/github-brands-solid.svg" alt="Icon de Github" className="logo"/>
+                            </a>
+                            <a href="https://twitter.com" target="_blank">
+                                <img src="../public/icon/square-twitter-brands-solid.svg" alt="Icon de Twitter" className="logo"/>
+                            </a>
+                            <a href="https://linkedin.com" target="_blank">
+                                <img src="../public/icon/linkedin-brands-solid.svg" alt="Icon de Linkedin" className="logo"/>
+                            </a>
+                        </div>
+                    </section>
+                    <section>
+                        <h5>Liens utiles</h5>
+                        <ul className="ps-3">
+                            <li><Link to="/">Accueil</Link></li>
+                            <li><Link to="/">à propos</Link></li>
+                            <li><Link to="/service">Services</Link></li>
+                            <li><Link to="/contact">Me contacter</Link></li>
+                            <li><Link to="/legal">Mentions légales</Link></li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h5>Mes dernières réalisations</h5>
+                        <ul className="ps-3">
+                            <li><Link to="/portfolio">Fresh food</Link></li>
+                            <li><Link to="/portfolio">Restaurant Akira</Link></li>
+                            <li><Link to="/portfolio">Espace bien-être</Link></li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h5>Mes derniers articles</h5>
+                        <ul className="ps-3">
+                            <li><Link to="/blog">Coder son site en HTML/CSS</Link></li>
+                            <li><Link to="/blog">Vendre ses produits sur le web</Link></li>
+                            <li><Link to="/blog">Se positionner sur Google</Link></li>
+                        </ul>
+                    </section>
                 </div>
-                <div>
-                    <a href="https://github.com" target="_blank">
-                        <img src="../public/icon/github-brands-solid.svg" alt="Icon de Github" className="logo"/>
-                    </a>
-                    <a href="https://twitter.com" target="_blank">
-                        <img src="../public/icon/square-twitter-brands-solid.svg" alt="Icon de Twitter" className="logo"/>
-                    </a>
-                    <a href="https://linkedin.com" target="_blank">
-                        <img src="../public/icon/linkedin-brands-solid.svg" alt="Icon de Linkedin" className="logo"/>
-                    </a>
+                <div className="d-flex align-items-center justify-content-center bg-dark mt-3 py-3">
+                    <img src="../public/icon/copyright-regular.svg" alt="signe copyright" style={{width:16}} />
+                    <span className="text-white ps-1">Designed by John Doe</span>
                 </div>
-            </section>
-            <section>
-                <h5>Liens utiles</h5>
-                <ul>
-                    <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/">à propos</Link></li>
-                    <li><Link to="/service">Services</Link></li>
-                    <li><Link to="/contact">Me contacter</Link></li>
-                    <li><Link to="/legal">Mentions légales</Link></li>
-                </ul>
-            </section>
-            <section>
-                <h5>Mes dernières réalisations</h5>
-                <ul>
-                    <li><Link to="/portfolio">Fresh food</Link></li>
-                    <li><Link to="/portfolio">Restaurant Akira</Link></li>
-                    <li><Link to="/portfolio">Espace bien-être</Link></li>
-                </ul>
-            </section>
-            <section>
-                <h5>Mes derniers articles</h5>
-                <ul>
-                    <li><Link to="/blog">Coder son site en HTML/CSS</Link></li>
-                    <li><Link to="/blog">Vendre ses produits sur le web</Link></li>
-                    <li><Link to="/blog">Se positionner sur Google</Link></li>
-                </ul>
-            </section>
-            <div>
-                <img src="../public/icon/copyright-regular.svg" alt="signe copyright" className="icon"/>
-                <span>Designed by John Doe</span>
             </div>
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,10 @@ const Home = () => {
                     <h2 className="mb-2 p-2 text-light ">Développeur web full stack</h2>
                     <button type="button" className="btn btn-primary">En savoir plus</button>
                 </div>
-                <section className="d-flex flex-column mx-4 my-5 px-4 py-5 container-about">
-                    <article >
-                        <h3 className="">À propos</h3>
-                        <p>Passioné par l'informatique et les nouvelles technologies,
+                <section className="d-flex flex-column my-5 p-5 container-about">
+                    <article className="fs-5">
+                        <h3 className="fs-2">À propos</h3>
+                        <p className="fs-5">Passioné par l'informatique et les nouvelles technologies,
                             j'ai suivi une formation d'<strong>intégrateur-développeur web </strong>
                             au CEF. Au cours de cette formation, j'ai pu acquérir des bases solides
                              pour travailler dans le domaine du <strong>développement web</strong>.
@@ -31,27 +31,27 @@ const Home = () => {
                         </p>
                     </article>
                     <div className="">
-                        <img src="../../public/img/about.jpg" alt="Photo de profil de John Doe" />
-                        <h4>Mes compétences</h4>
-                            <div>
+                        <img src="../../public/img/john-doe-about.jpg" alt="Photo de profil de John Doe" className="image-profil"/>
+                        <h4 className="my-1 py-1">Mes compétences</h4>
+                            <div className="">
                                 <span>HTML5 90%</span>
-                                <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress mb-2" role="progressbar" aria-label="Danger example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
                                     <div className="progress-bar bg-danger w-90" style={{width:'90%'}}></div>
                                 </div>
                                 <span>CSS3 80%</span>
-                                <div className="progress" role="progressbar" aria-label="Info example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress mb-2" role="progressbar" aria-label="Info example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
                                     <div className="progress-bar bg-info" style={{width:'80%'}}></div>
                                 </div>
                                 <span>JAVASCRIPT 70%</span>
-                                <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress mb-2" role="progressbar" aria-label="Warning example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
                                     <div className="progress-bar bg-warning" style={{width:'70%'}}></div>
                                 </div>
                                 <span>PHP 60%</span>
-                                <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress mb-2" role="progressbar" aria-label="Success example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
                                     <div className="progress-bar bg-success" style={{width:'25%'}}></div>
                                 </div>
                                 <span>REACT 50%</span>
-                                <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress mb-2" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                                     <div className="progress-bar bg-basic" style={{width:'50%'}}></div>
                                 </div>
                             </div>
@@ -61,7 +61,7 @@ const Home = () => {
             <footer>
                 <Footer/>
             </footer>
-       </>
+        </>
     )
 }
 
